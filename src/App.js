5f039933ae4c788a9d6650d7ab1489bf8873b29d
Hector Galindo <hectordaniel@gmail.com>
2022-02-10 12:49:22 -0500

    consumiendo mi aoi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Buscador from './componentes/Buscador';
+import Resultado from './componentes/Resultado';
 
-function App() {
+class App extends Component {
+
+  state = {
+    termino : '',
+    imagenes : []
+  }
+
+  paginaAnterior = () => {
+    console.log('anterior...');
+  }
+
+  paginaSiguiente = () => {
+    console.log('siguiente...');
+  }
+
+  consultarApi = () => {
+    const termino =  this.state.termino;
+    const url = `https://pixabay.com/api/?key=1732750-d45b5378879d1e877cd1d35a6&q=${termino}&per_page30`;
+
+    fetch(url)
+    .then(respuesta => respuesta.json())
+    .then(resultado => this.setState({ imagenes : resultado.hits }))
+  }
+
+
+
+  datosBusqueda = (termino) => {
+    this.setState({
+      termino
+    }, () => {
+      this.consultarApi();
+    })
+    
+  }
+  
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app container">
+      <div className="jumbotron">
+        <p className="lead text-center">Buscador de Imágenes</p>
+
+        <Buscador 
+        
+        datosBusqueda={this.datosBusqueda}
+        />
+      </div>
+    <div className="row justify-content-center">
+      <Resultado 
+       imagenes={this.state.imagenes}
+       paginaAnterior={this.paginaAnterior}
+       paginaSiguiente={this.paginaSiguiente}
+      />
+    </div>
     </div>
   );
+}
 }
 
 export default App;
